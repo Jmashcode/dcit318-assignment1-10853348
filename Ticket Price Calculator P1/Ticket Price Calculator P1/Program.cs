c375ebe61001a979d67f9fcd53349e0c67c7d2df
Joshua <joshuagomashie@gmail.com>
2024-06-29 00:37:45 -0400

    Completed Ticket Calculator

---
+++ Ticket Price Calculator P1/Ticket Price Calculator P1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int standardPrice = 10;
+            const int discountedPrice = 7;
+
+            Console.Write("Enter your age: ");
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                if (age <= 12 || age >= 65)
+                {
+                    Console.WriteLine($"Ticket Price: GHC{discountedPrice}");
+                }
+                else
+                {
+                    Console.WriteLine($"Ticket Price: GHC{standardPrice}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid age.");
+            }
+        }
+    }
+}
